2a785f53608be751b445d09443d0c75f19a575e5
Ramesh lal das <lalrameshdas@gmail.com>
2025-11-26 04:53:16 +0530

    first nodejs code

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = 20;
+const b = 30;
+var c = 40;
+
+console.log(a + b + c);
+
+console.log(10 + 20);
+
+let fs = require("fs");
+const text = fs.readFileSync("./text.txt");
+
+console.log(text);
+console.log(text.toString());
